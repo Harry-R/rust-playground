16bf9b12c3b14c66901aa8ddd2b523942566df85
Leonard Rapp <leonardrapp@gmx.de>
2018-04-11 20:16:05 +0200

    sd: finisshed first minimal version

--- shadowdice/src/main.rs
+++ shadowdice/src/main.rs
@@ -3,6 +3,9 @@
 **/
 
 use std::io;
+use rand::{Rng, thread_rng};
+
+extern crate rand;
 
 fn main() {
     let mut dicepool : i8 = 0;
@@ -16,8 +19,32 @@ fn main() {
 
     match input.trim().parse::<i8> () {
         Ok(val) => dicepool = val,
-        Err(e) => println!("Please enter a number < 255!"),
+        Err(e) => println!("Please enter a number < 255: {}", e),
     }
-    
-    println!("Dicpool size is {}.", dicepool);
+
+    // define thread_rng as randomness source
+    let mut rng = thread_rng();
+
+    let mut hits : i8 = 0;
+    let mut fails : i8 = 0;
+
+    // roll dices
+    for x in 1..dicepool+1 {
+        let randnr = rng.gen_range(1, 7);
+        if randnr >= 4 {
+            hits += 1;
+        } else if randnr == 1{
+            fails += 1;
+        }
+        println!("dice nr {} is {}", x, randnr);
+    }
+    // evaluate dices
+    println!("hits: {}  fails: {}", hits, fails);
+    if fails > dicepool / 2 {
+        if hits == 0 {
+            println!("CRITICAL GLITCH!");
+        } else {
+            println!("GLITCH");
+        }
+    }
 }
